a0bf292eb1bfb0e44d7ef640074c6b1547151883
pratiksayanekar <pratiksayanekar26@gmail.com>
2020-12-22 13:38:31 +0000

    first solution - brute force
    execution time exceeded..

--- src/HappyNumbers.java
+++ src/HappyNumbers.java
@@ -1,9 +1,32 @@
 
 public class HappyNumbers {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static boolean isHappy(int n) {
+		if(n>0) {
+			int sum = 0;
+			while (sum!=1) {
+				sum = 0;
+				int rem = n%10;
+				sum += rem*rem;
+				int que = n/10;
+				while(que>0) {
+					rem = que%10;
+					sum += rem*rem;
+					que /= 10;
+				}
+				n=sum;
+				System.out.println(sum);
+			}
+			if(sum==1)
+				return true;
+			else 
+				return false;
+		}
+		return false;
+    }
 
+	public static void main(String[] args) {
+		System.out.println(isHappy(2));
 	}
 
 }
