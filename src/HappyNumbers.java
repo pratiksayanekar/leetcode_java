85e368c0cba9f2a11edfbd0a90a77979948333cf
pratiksayanekar <pratiksayanekar26@gmail.com>
2020-12-22 13:49:24 +0000

    optimized solution

--- src/HappyNumbers.java
+++ src/HappyNumbers.java
@@ -1,11 +1,13 @@
+import java.util.HashSet;
 
 public class HappyNumbers {
 
 	public static boolean isHappy(int n) {
+		HashSet<Integer> set = new HashSet<Integer>();
 		if(n>0) {
-			int sum = 0;
-			while (sum!=1) {
-				sum = 0;
+			while (!set.contains(n)) {
+				set.add(n);
+				int sum = 0;
 				int rem = n%10;
 				sum += rem*rem;
 				int que = n/10;
@@ -15,18 +17,15 @@ public class HappyNumbers {
 					que /= 10;
 				}
 				n=sum;
-				System.out.println(sum);
+				if(sum==1)
+					return true;
 			}
-			if(sum==1)
-				return true;
-			else 
-				return false;
 		}
 		return false;
     }
 
 	public static void main(String[] args) {
-		System.out.println(isHappy(2));
+		System.out.println(isHappy(19));
 	}
 
 }
